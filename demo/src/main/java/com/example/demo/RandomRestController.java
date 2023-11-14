1646365f220fb45850a1bf046acb4b7fd66cc36c
Paulo Barreto Neto <105065779+PauloBarretoDev@users.noreply.github.com>
2023-11-14 22:46:54 +0000

    aula

---
+++ demo/src/main/java/com/example/demo/RandomRestController.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomRestController {
+    @GetMapping(value="/random")
+    public int getRandom(
+        @RequestParam(value="min", required=false, defaultValue= "1") String minStr,
+        @RequestParam(value="max",required = false, defaultValue ="100") String maxStr)
+        {
+            int min = 1;
+            int max = 101;
+            try{
+                min = Integer.parseInt(minStr);
+            }catch (NumberFormatException ex){
+                min = 1;
+            }
+            try{
+                max = Integer.parseInt(maxStr);
+            }catch (NumberFormatException ex){
+                max = 101;
+            }
+            if(max>min){
+                int aux = min;
+                min = max;
+                max = aux;
+            }
+            return (int) (Math.random() * (max - min + 1)) + min;
+
+        }  
+    {
+    }
+}
